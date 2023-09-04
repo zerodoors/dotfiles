93b8cda4f24400a253adb64de93ee2d305970cd7
zerodoors <git@zerodoors.dev>
2023-09-04 17:52:21 -0400

    Add neovim config

---
+++ dot_config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,14 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "rockerBOO/boo-colorscheme-nvim" },
+  { "EdenEast/nightfox.nvim" },
+  { "mcchrish/zenbones.nvim" },
+  { "rktjmp/lush.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "forest_stream",
+    },
+  },
+}
